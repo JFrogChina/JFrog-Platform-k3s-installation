0aaf829371c91b4bbcbed72e465d2124c18a3cfb
jfrogpz <someone@example.com>
2025-07-19 10:47:01 +0800

    add more help info

--- login-pg-pod.sh
+++ login-pg-pod.sh
@@ -4,11 +4,27 @@ source common.sh
 
 kubectl exec --stdin --tty jfrog-platform-postgresql-0 -n $NAMESPACE -- /bin/bash
 
-# after login
 
-# e.g.
+# after login
 # df -h
 # du -sh /bitnami/postgresql/data/
+
+
+# default
+# postgresqlUsername: postgres
+# postgresqlPassword: postgres
+
+# user: artifactory
+# password: artifactory
+
+# user: xray
+# password: xray
+
+# e.g.
+# psql -U xray -d xray -p 5432
+
+
+
 
 
 
