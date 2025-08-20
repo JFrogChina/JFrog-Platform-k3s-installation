aa07d29469a4da05fa693707c0d6fe26dd7024b2
jfrogpz <someone@example.com>
2025-08-20 17:57:02 +0800

    add extra system yaml

---
+++ login-catalog-pod.sh
@@ -0,0 +1,26 @@
+
+# include common
+source common.sh
+
+POD_PREFIX=jfrog-platform-catalog
+
+# 获取最新 pod 名称
+POD_NAME=$(kubectl get pods -n $NAMESPACE \
+  | grep "^$POD_PREFIX" \
+  | awk '{print $1}' \
+  | head -n 1)
+
+if [ -z "$POD_NAME" ]; then
+  echo "未找到 Pod: $POD_PREFIX"
+  exit 1
+fi
+
+kubectl exec --stdin --tty $POD_NAME -n $NAMESPACE -- /bin/bash
+
+# after login
+
+# e.g.
+# cd /opt/jfrog/catalog/var/etc
+# cat ./system.yaml
+
+
