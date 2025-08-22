1cdc7bff00b9af7364f6cc99d5464ced54c2c628
jfrogpz <someone@example.com>
2025-08-22 18:29:04 +0800

    remove chinese, only english

--- check-catalog-log.sh
+++ check-catalog-log.sh
@@ -4,16 +4,34 @@ source common.sh
 
 POD_PREFIX=jfrog-platform-catalog
 
-# 获取最新 pod 名称
 POD_NAME=$(kubectl get pods -n $NAMESPACE \
   | grep "^$POD_PREFIX" \
   | awk '{print $1}' \
   | head -n 1)
 
 if [ -z "$POD_NAME" ]; then
-  echo "未找到 Pod: $POD_PREFIX"
+  echo "cannot find Pod: $POD_PREFIX"
   exit 1
 fi
 
-echo "正在查看 Pod 日志: $POD_NAME"
+echo "checking log of Pod: $POD_NAME"
 kubectl logs -f pod/$POD_NAME -n $NAMESPACE
+
+# also check health by browser
+# curl http://x.x.x.x:8080/catalog/api/v1/system/app_health
+
+# {
+#     "entitlements": {
+#         "entitled_for_catalog": true,
+#         "has_central_token": true,
+#         "token_expired": false,
+#         "token_expiration": "2025-08-22T16:22:10Z"
+#     },
+#     "central": {
+#         "central_connection_working": true,
+#         "central_node": "catalog-central-584fbf78b-kv4vw"
+#     },
+#     "db_connection_working": true,
+#     "one_model_available": true,
+#     "code": "OK"
+# }
